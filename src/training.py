3d2c97c294062651e189d1ad2ee9c9bdd96b9f23
jsp566 <antonni9801@gmail.com>
2025-11-23 11:44:31 +0100

    logger

--- src/training.py
+++ src/training.py
@@ -2,14 +2,16 @@ import numpy as np
 
 
 class Trainer:
-    def __init__(self, model, loss_function, optimizer=None):
+    def __init__(self, model, loss_function, optimizer=None, logger=None):
         self.model = model
         self.loss_function = loss_function
         self.optimizer = optimizer
+        self.logger = logger
 
     def train(self, X, y, epochs=10, batch_size=32, shuffle=True):
         num_samples = X.shape[0]
         history = {"loss": []}
+        
 
         for epoch in range(epochs):
             if shuffle:
@@ -43,9 +45,14 @@ class Trainer:
 
             epoch_loss /= batches
             history["loss"].append(epoch_loss)
+            # Log metrics
+            if self.logger is not None:
+                self.logger.log_metrics(epoch, y, self.model.forward(X))
 
             print(f"Epoch {epoch+1}/{epochs} - Loss: {epoch_loss:.4f}")
 
+        if self.logger is not None:
+            self.logger.finish()
         return history
 
     def predict(self, X):
---
+++ src/wandblogger.py
@@ -0,0 +1,55 @@
+import wandb
+
+
+# config should be a dictionary containing hyperparameters and settings
+# initializer
+# number of layers
+# nodes per layer
+# loss function
+# optimizer
+# learning rate
+# batch size
+# epochs
+# dataset
+
+
+
+class Logger:
+    def __init__(self, project, config, model, loss_function):
+        self.project = project
+        self.config = config
+        self.model = model
+        self.loss_function = loss_function
+        self.run = wandb.init(
+            project=project,
+            entity="DTU-Deep-Learning-Project",
+            config=config,
+        )
+        #self.run.define_metric("*", step_metric="epoch")
+
+    def initialize(self):
+        self.run = wandb.init(
+            project=self.project,
+            entity="DTU-Deep-Learning-Project",
+            config=self.config,
+        )
+
+    def log_metrics(self, epoch, y_true, y_pred):
+        #train_loss = self.loss_function.compute(y_true, y_pred)
+        metrics = {"train/loss": None,
+                   "train/accuracy": None,
+                   "val/loss": None,
+                   "val/accuracy": None
+                   }
+        
+        for i, layer in enumerate(self.model.layers):
+            metrics[f"weights/{str(i)}"] = layer.weights
+            metrics[f"biases/{str(i)}"] = layer.biases
+            metrics[f"grad_weights/{str(i)}"] = layer.grad_weights
+            metrics[f"grad_biases/{str(i)}"] = layer.grad_biases
+
+        
+        self.run.log(metrics, step=epoch)
+
+    def finish(self):
+        self.run.finish()
